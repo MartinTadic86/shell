407cc1bc0e0344f1fc63ef2d227304ae16efb583
Martin Tadič <96323454+MartinTadic86@users.noreply.github.com>
2023-05-10 08:34:19 +0200

    shrnuti.sh bez curl

--- shrnuti.sh
+++ shrnuti.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-# Kontrola argumentů
-if [ $# -eq 0 ]
-  then
-    echo "Není zadaná adresa URL."
-    exit 1
+if [ $# -eq 0 ]; then
+  echo "Není zadaná adresa URL."
+  exit 1
 fi
 
-# Uložení URL adresy do proměnné
 URL=$1
 
-# Zobrazení URL adresy v terminálu
 echo "Zadaná URL adresa: $URL"
 
-# Stažení HTML dokumentu ze zadané URL adresy
-curl -s $URL > dokument.html
+wget -q -O dokument.html $URL
 
-# Výběr odstavců pomocí regulárních výrazů
+if [ $? -ne 0 ]; then
+  echo "Stažení stránky selhalo."
+  exit 1
+fi
+
 odstavce=$(grep -oP '<p.*?>.*?</p>' dokument.html)
 
-# Zobrazení vybraných odstavců v terminálu
 echo "Vybrané odstavce:"
 echo $odstavce
 
-# Uložení vybraných odstavců do souboru
 echo $odstavce > odstavce.txt
 
-# Komprimace souboru pomocí gzip
 gzip odstavce.txt
 
-# Zobrazení zprávy o úspěšném dokončení skriptu
 echo "Skript byl úspěšně dokončen."
 
